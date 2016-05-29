7ea58eb943265d3448f88e89ce09d3bf794f01e8
jerinviju <jerinviju.mec@gmail.com>
2016-05-29 18:12:13 +0530

    second commit

--- excel2016/app/src/main/java/com/example/jerin/excel2016/fragments/galleryfragment.java
+++ excel2016/app/src/main/java/com/example/jerin/excel2016/fragments/galleryfragment.java
@@ -6,19 +6,76 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.LinearLayout;
+import android.widget.TextSwitcher;
+import android.widget.TextView;
+import android.widget.ViewSwitcher;
 
+import com.example.jerin.excel2016.MainActivity;
 import com.example.jerin.excel2016.R;
+import com.example.jerin.excel2016.adapter.CoverFlowAdapter;
+import com.example.jerin.excel2016.pojo.gallerypojo;
+import com.special.ResideMenu.ResideMenu;
+
+import java.util.ArrayList;
+
+import it.moondroid.coverflow.components.ui.containers.FeatureCoverFlow;
 
 /**
  * Created by jerin on 6/5/16.
  */
 public class galleryfragment extends Fragment{
 
+    private FeatureCoverFlow mCoverFlow;
+    private CoverFlowAdapter mAdapter;
+    private ArrayList<gallerypojo> mData = new ArrayList<>(0);
+    private TextView mTitle,mtitle1;
+    LinearLayout linearLayout;
+
+    private ResideMenu resideMenu;
+
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View  view = inflater.inflate(R.layout.galleryfragment, container, false);
+
+        MainActivity parentActivity = (MainActivity) getActivity();
+        resideMenu = parentActivity.getResideMenu();
+
+        mData.add(new gallerypojo(getResources().getDrawable(R.drawable.image_1), "poda patty","how do u do"));
+        mData.add(new gallerypojo(getResources().getDrawable(R.drawable.image_2),"pulle","how are u"));
+        mData.add(new gallerypojo(getResources().getDrawable(R.drawable.image_3),"hello","how is your leg"));
+        mData.add(new gallerypojo(getResources().getDrawable(R.drawable.image_4),"hehehe","how old are you"));
+
+        mTitle = (TextView) view.findViewById(R.id.title);
+
+        mtitle1 = (TextView) view.findViewById(R.id.title2);
+
+
+
+
+        mAdapter = new CoverFlowAdapter(getActivity());
+        mAdapter.setData(mData);
+        mCoverFlow = (FeatureCoverFlow) view.findViewById(R.id.coverflow);
+        mCoverFlow.setAdapter(mAdapter);
+
+        resideMenu.addIgnoredView(mCoverFlow);
+        mCoverFlow.setOnScrollPositionListener(new FeatureCoverFlow.OnScrollPositionListener() {
+            @Override
+            public void onScrolledToPosition(int position) {
+                mTitle.setText(mData.get(position).getUploader());
+                mtitle1.setText(mData.get(position).getUploaderscomment());
+            }
+
+            @Override
+            public void onScrolling() {
+                mTitle.setText("");
+                mtitle1.setText("");
+            }
+        });
 
         return  view;
     }
--- excel2016/app/src/main/java/com/example/jerin/excel2016/pojo/gallerypojo.java
+++ excel2016/app/src/main/java/com/example/jerin/excel2016/pojo/gallerypojo.java
@@ -13,27 +13,36 @@ public class gallerypojo {
     String uploader;
     String nooflikes;
     String uploaderscomment;
-    Drawable getimage(){
-        return image;
+
+    public gallerypojo(Drawable image,String name,String comment){
+        this.image=image;
+        this.uploader=name;
+        this.uploaderscomment=comment;
+
     }
+
+
     public Drawable getImage(){
         return image;
     }
     public void setimage(Drawable image){
         this.image=image;
     }
+
     public String getUploader(){
         return uploader;
     }
     public void setUploader(String uploader){
         this.uploader=uploader;
     }
+
    public  String getNooflikes(){
         return nooflikes;
     }
     public  void setnooflikes(String nooflikes){
         this.nooflikes=nooflikes;
     }
+
     public String getUploaderscomment(){
         return uploaderscomment;
     }
